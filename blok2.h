62597e4a284150e9682ce2e1537954602a164fe4
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-04-02 17:56:43 +0200

    table function declaration

--- blok2.h
+++ blok2.h
@@ -5,3 +5,6 @@ void uloha2_1_2();
 void uloha2_1_3();
 void uloha2_1_4();
 void uloha2_5_1();
+void uloha2_5_2();
+void genTable(int rightWidth, int posX, int posY);
+void tableHead(int rightWidth, int posX, int posY);
